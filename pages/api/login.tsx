521e9a76e450bf83324b306d4ab0457101dec04b
dydwn6565 <55012173+dydwn6565@users.noreply.github.com>
2022-10-20 22:13:36 -0700

    changed redirecturl in login.tsx

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -4,7 +4,8 @@ export default async function login(req, res) {
     const code = req.body.code;
 
     const spotifyApi = new SpotifyWebApi({
-      redirectUri: "http://localhost:3000",
+      // redirectUri: "http://localhost:3000",
+      redirectUri: "https://spotifyclone1-tau.vercel.app",
       clientId: "481fa5ea109645d9b9e6dc0e596d50d6",
       clientSecret: "280b7c09342d47ef9cc4b0b1727d6903",
     });
